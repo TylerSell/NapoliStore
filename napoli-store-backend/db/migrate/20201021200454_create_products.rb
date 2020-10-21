e96b1d6c7d08388eb4ea72c76baa9d196449e38e
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-21 16:03:48 -0500

    update Products migration for price column

--- napoli-store-backend/db/migrate/20201021200454_create_products.rb
+++ napoli-store-backend/db/migrate/20201021200454_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.float :price
+      t.decimal :price, precision: 5, scale: 2, default: 0
       t.string :description
       t.string :detail_1
       t.string :detail_2
