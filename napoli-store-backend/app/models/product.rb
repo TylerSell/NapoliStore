9572dfb3a386bbcb0439c13aab638605a0540507
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-21 17:23:58 -0500

    add validations to Product model

--- napoli-store-backend/app/models/product.rb
+++ napoli-store-backend/app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
+
   belongs_to :collection
   belongs_to :category
+
+  validates :name, presence => true
+  validates :description, presence => true, length => { maximum => 1000 }
+  validates :price, numericality => { greater_than_or_equal_to => 0 }
+  validates :quantity_sm, :quantity_med, :quantity_large, :quantity_xl, numericality => { greater_than_or_equal_to => 0 }
+  validates :detail_1, :detail_2, :detail_3, :detail_4, :detail_5, :detail_6, length => { maximum => 1000 }, allow_nil => true 
+
+
 end
