0b7197e850f9d077a502fa68d8fa9e2c3e9292fd
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-21 15:05:18 -0500

    add Product resource and migration

---
+++ napoli-store-backend/db/migrate/20201021200454_create_products.rb
@@ -0,0 +1,23 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :price
+      t.string :description
+      t.string :detail_1
+      t.string :detail_2
+      t.string :detail_3
+      t.string :detail_4
+      t.string :detail_5
+      t.string :detail_6
+      t.string :quantity_sm
+      t.string :quantity_med
+      t.string :quantity_large
+      t.string :quantity_xl
+      t.references :collection, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
