a355fac756c3fc30fd574f3ded42af7e296556b1
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-26 10:19:38 -0500

    start building session controller

--- napoli-store-backend/app/controllers/session_controller.rb
+++ napoli-store-backend/app/controllers/session_controller.rb
@@ -1,2 +1,26 @@
 class SessionController < ApplicationController
+    def create
+        @user = User.find_by(email: params[:email])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            render json: @user 
+        else 
+            render json: {error: "Invalid Credentials. Please Try Again."}
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user, serializer: UserSerializer
+        else 
+            render json: { error: "No current User logged in."}
+        end
+    end
+
+
+    def destroy
+        session.clear
+        render json: { message: "Successfully logged out" }
+    end
 end
