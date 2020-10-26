1e9f46e736cae5077b0b6cb0f0572ef25dccc033
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-26 10:01:47 -0500

    build validation for url string

--- napoli-store-backend/app/models/product.rb
+++ napoli-store-backend/app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ApplicationRecord
   validates :price, numericality => { greater_than_or_equal_to => 0 }
   validates :quantity_sm, :quantity_med, :quantity_large, :quantity_xl, numericality => { greater_than_or_equal_to => 0 }
   validates :detail_1, :detail_2, :detail_3, :detail_4, :detail_5, :detail_6, length => { maximum => 1000 }, allow_nil => true 
+  validates :photo, url => true
 
 
 end
