ee454154ff6886a44f57975f7bc8a98f37e4f148
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-26 09:54:59 -0500

    add photo url field to products db

--- napoli-store-backend/db/migrate/20201021200454_create_products.rb
+++ napoli-store-backend/db/migrate/20201021200454_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :name
+      t.string :photo
       t.decimal :price, precision: 8, scale: 2, default: 0
       t.string :description
       t.string :detail_1
