b3f92bf900a61bdf12902fa5621e8a61f05ec389
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-26 10:06:57 -0500

    update product serializer

--- napoli-store-backend/app/serializers/product_serializer.rb
+++ napoli-store-backend/app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description, :detail_1, :detail_2, :detail_3, :detail_4, :detail_5, :detail_6, :quantity_sm, :quantity_med, :quantity_large, :quantity_xl
+  attributes :id, :photo , :name, :price, :description, :detail_1, :detail_2, :detail_3, :detail_4, :detail_5, :detail_6, :quantity_sm, :quantity_med, :quantity_large, :quantity_xl
   has_one :collection
   has_one :category
 end
