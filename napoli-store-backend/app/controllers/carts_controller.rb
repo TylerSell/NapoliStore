9bb5ee2cedc72d87799e5e389ee9d07831417ba6
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-26 16:36:46 -0500

    update carts controller

--- napoli-store-backend/app/controllers/carts_controller.rb
+++ napoli-store-backend/app/controllers/carts_controller.rb
@@ -1,2 +1,9 @@
 class CartsController < ApplicationController
+    def index
+        cart = Cart.find_by(id: params[:id])
+        products = cart.cart_items
+        render json: products
+    end
+
+    
 end
