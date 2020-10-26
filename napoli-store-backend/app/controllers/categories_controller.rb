33dae38d012a994698d290ede75804dd18ff9a64
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-10-26 16:19:48 -0500

    build out more controllers

--- napoli-store-backend/app/controllers/categories_controller.rb
+++ napoli-store-backend/app/controllers/categories_controller.rb
@@ -5,8 +5,20 @@ class CategoriesController < ApplicationController
     end
 
     def create
+        if is_admin?
+            category = Category.build(category_params)
+            render json: category.save ? category : {error: 'Something went wrong please try again'}
+        else 
+            render json: {error: 'You do not sufficient privelidges to do this. Please ask an Admin for assistance'}
+        end
     end
 
-    def update
+    # ---------------------------------------------------
+
+    private 
+
+    def category_params
+        params.require(:category).permit(:name)
     end
+
 end
--- napoli-store-backend/app/controllers/collections_controller.rb
+++ napoli-store-backend/app/controllers/collections_controller.rb
@@ -5,10 +5,8 @@ class CollectionsController < ApplicationController
     end
 
     def create
-        collection = Collection.find_by(id: params[:id])
-
         if is_admin?
-            collection.build(collection_params)
+            collection = Collection.build(collection_params)
             render json: collection.save ? collection : {error: 'Something went wrong please try again'}
         else 
             render json: {error: 'You do not sufficient privelidges to do this. Please ask an Admin for assistance'}
