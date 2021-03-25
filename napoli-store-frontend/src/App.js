c35301c0f24ff825d68bb70a6ce653b8d0cef97d
TylerSell <41409967+TylerSell@users.noreply.github.com>
2021-03-25 18:16:30 -0500

    add Link and React Router

--- napoli-store-frontend/src/App.js
+++ napoli-store-frontend/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/">
+              <Navbar />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- napoli-store-frontend/src/components/Navbar.js
+++ napoli-store-frontend/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
 
 class Navbar extends Component {
     render() {
       return(
         <header className='navbar'>
-            <div className='navbar__title navbar__item'>THENAPOLISTORE</div>
-            <div className='navbar__item'>HOME</div>
-            <div className='navbar__item'>CATALOG</div>
-            <div className='navbar__item'>ACCOUNT</div>
-            <div className='navbar__item navbar__end'>CART</div>        
+          <Link to="/" className='navbar__title navbar__item'>
+            <img src="https://i.ibb.co/W09jqpN/Napoli-Black-Logo.png" alt="Napoli Logo" className="header__logo"/>
+          </Link>
+            <Link to="/" className='navbar__item'>HOME</Link>
+            <Link to="/catalog" className='navbar__item'>CATALOG</Link>
+            <Link to="/account" className='navbar__item'>ACCOUNT</Link>
+            <Link to="/cart" className='navbar__item navbar__end'>CART</Link>        
         </header>
       )
     }
