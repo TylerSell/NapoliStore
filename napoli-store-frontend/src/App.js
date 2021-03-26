cb5f555ea9a902f16ffd653342eb2408ae71a43e
TylerSell <41409967+TylerSell@users.noreply.github.com>
2021-03-26 02:24:00 -0500

    building hero banner and text animation

--- napoli-store-frontend/src/App.js
+++ napoli-store-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Banner from './components/Banner';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
           <Switch>
             <Route path="/">
               <Navbar />
+              <Banner />
               <Footer />
             </Route>
           </Switch>
---
+++ napoli-store-frontend/src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div className="hero__banner">
+            <img className="hero__img" src="//cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_300x.jpeg?v=1597368641" data-widths="[540, 720, 900, 1080, 1296, 1512, 1728, 1944, 2048, 4472]" data-aspectratio="1.4351786965662228" data-sizes="auto" data-parent-fit="cover" data-hero-image="" alt="" data-srcset="//cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_540x.jpeg?v=1597368641 540w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_720x.jpeg?v=1597368641 720w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_900x.jpeg?v=1597368641 900w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1080x.jpeg?v=1597368641 1080w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1296x.jpeg?v=1597368641 1296w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1512x.jpeg?v=1597368641 1512w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1728x.jpeg?v=1597368641 1728w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1944x.jpeg?v=1597368641 1944w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_2048x.jpeg?v=1597368641 2048w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_4472x.jpeg?v=1597368641 4472w" sizes="1905px" srcSet="//cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_540x.jpeg?v=1597368641 540w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_720x.jpeg?v=1597368641 720w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_900x.jpeg?v=1597368641 900w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1080x.jpeg?v=1597368641 1080w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1296x.jpeg?v=1597368641 1296w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1512x.jpeg?v=1597368641 1512w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1728x.jpeg?v=1597368641 1728w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_1944x.jpeg?v=1597368641 1944w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_2048x.jpeg?v=1597368641 2048w, //cdn.shopify.com/s/files/1/0456/8426/1016/files/072D779A-7FFF-4218-9650-C486B9A89F99_4472x.jpeg?v=1597368641 4472w"/>
+            <div className="hero__textWrapper">
+                <div className="hero__textCenterContainer">
+                    <div className="hero__textContent">
+                        <p className="hero__subtitle">FEATURING MERCH BY</p>
+                        <p className="hero__title">ARCH APPAREL</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
